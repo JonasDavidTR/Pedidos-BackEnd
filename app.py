13a340ebb48ff7153ef93c631f4b5117039c90c6
JonasDavid <90807568+JonasDavidTR@users.noreply.github.com>
2025-05-25 03:26:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -149,8 +149,7 @@ def enviar_pedido():
 
         # Escapa para URL
         mensagem_url = quote(mensagem)
-
-        # Remove caracteres especiais do número
+        #  Numero a para qual a mensagem será mandada
         numero = '5587981796957'
 
         # Salva na planilha
