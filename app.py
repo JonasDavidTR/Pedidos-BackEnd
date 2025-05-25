5ab0ad1c6fc83f022437a6a406bd5b322007beeb
JonasDavid <90807568+JonasDavidTR@users.noreply.github.com>
2025-05-25 16:46:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def enviar_pedido():
         # Escapa para URL
         mensagem_url = quote(mensagem)
         #  Numero a para qual a mensagem será mandada
-        numero = '5587981796957'
+        numero = '5587981554312'
 
         # Salva na planilha
         sheet.append_row([timestamp, whatsapp, pedido, endereco, pagamento])
