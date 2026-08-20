688cd570ef6d59b2371a0e1f8569702cd858304b
JonasDavid <90807568+JonasDavidTR@users.noreply.github.com>
2026-08-20 18:59:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ precos = {
     'pastel': 7.00,
     'coxinha': 5.00,
     'enroladinho': 3.00,
-    'hotdog': 6.00,
+    'hotdog': 7.00,
     'bolo': 6.00,
     'tortilete': 3.00,
     'trufa': 3.00,
@@ -161,8 +161,8 @@ precos = {
     'sucoAgua': 3.50,
     'coca': {'Juininho': 3.00, 'Lata': 5.00, '1L': 8.00, '2L': 13.00},
     'fanta': {'Juininho': 3.00, 'Lata': 5.00, '1L': 8.00, '2L': 13.00},
-    'guarana': {'Juininho': 2.00, 'Lata': 4.00, '1L': 7.00, '2L': 12.00},
-    'outros': {'Juininho': 2.00, 'Lata': 4.00, '1L': 7.00, '2L': 12.00}  # pepsi/uva/limao
+    'guarana': {'Juininho': 3.00, 'Lata': 4.00, '1L': 7.00, '2L': 12.00},
+    'outros': {'Juininho': 3.00, 'Lata': 4.00, '1L': 7.00, '2L': 12.00}  # pepsi/uva/limao
 }
 
 @app.route("/calcular-valor", methods=["POST"])
